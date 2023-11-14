257b50bee55651a4a5b117a7af8602771a971a5b
Mike CTO <hh1@nimbusintelligence.com>
2023-11-14 13:38:33 +0000

    Fixed fct and stg tables to remove duplicates. Updated tests.

--- models/marts/sql/fct_videos_analytics.sql
+++ models/marts/sql/fct_videos_analytics.sql
@@ -24,53 +24,35 @@ video_info as (
 
 ),
 
-analytics as (
+final as (
 
     select
 
-        video_info.video_id as video_id
-        , sum(comment_count.comment_count) as comment_count
-        , sum(view_count.view_count) as view_count
-        , sum(likes_count.likes_count) as likes_count
-        , sum(video_info.dislikes + likes_count.dislikes_count) as dislikes_count
-
-    from video_info
-    left join comment_count
-        on video_info.video_id = comment_count.video_id
-    left join view_count
-        on video_info.video_id = view_count.video_id
-    left join likes_count
-        on video_info.video_id = likes_count.video_id
-    
-    group by video_info.video_id
-
-),
-
-final as (
-
-    select 
-    
-        analytics.video_id
-        , title
+        video_info.video_id
+        , video_info.trending_date
+        
         , published_date
+        , title
         , channel_id
         , channel_title
         , category_id
-        , trending_date
         , tags
         , thumbnail_link
         , comments_disabled
         , ratings_disabled
         , description
-        , likes_count
-        , dislikes_count
-        , comment_count
-        , view_count
-    
-    from analytics
-    left join video_info
-        on analytics.video_id = video_info.video_id
+
+        , comment_count.comment_count as comment_count
+        , view_count.view_count as view_count
+        , likes_count.likes_count as likes_count
+        , video_info.dislikes_count + likes_count.dislikes_count as dislikes_count
+
+    from video_info
+    left join comment_count using (video_id, trending_date)
+    left join view_count using (video_id, trending_date)
+    left join likes_count using (video_id, trending_date)
 
 )
 
 select * from final
+order by video_id, trending_date
--- models/staging/sql/stg_likes_count.sql
+++ models/staging/sql/stg_likes_count.sql
@@ -12,8 +12,8 @@ transformed as (
         
         value:video_id_for_client_HyPeHoUsE::string as video_id
         , {{ get_date_from_path('file_path') }} as trending_date
-        , value:likes_for_client_HyPeHoUsE as likes
-        , value:dislikes_for_client_HyPeHoUsE as dislikes
+        , value:likes_for_client_HyPeHoUsE as likes_count
+        , value:dislikes_for_client_HyPeHoUsE as dislikes_count
     
     from youtube_raw.public.likes_count_json
         , lateral flatten(input => json_data:data)
@@ -26,8 +26,8 @@ final as (
 
         video_id
         , trending_date
-        , {{ cast_to_number('likes') }} as likes_count
-        , {{ cast_to_number('dislikes') }} as dislikes_count
+        , {{ cast_to_number('likes_count') }} as likes_count
+        , {{ cast_to_number('dislikes_count') }} as dislikes_count
 
     from transformed
     group by 
