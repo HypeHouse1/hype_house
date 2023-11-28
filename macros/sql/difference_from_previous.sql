00739768bb25f7f16fcb9077835cd8b995bf67a8
Luca Balduzzi <luca.balduzzi@nimbusintelligence.com>
2023-11-28 15:25:19 +0100

    Display only non consecutive dates

--- macros/sql/difference_from_previous.sql
+++ macros/sql/difference_from_previous.sql
@@ -1,6 +1,6 @@
 {% macro difference_from_previous(column_name, partition_by, order_by) %}
 
-    {{ column_name }} - lag({{ column_name }}, 1, 0) over (
+    {{ column_name }} - lag({{ column_name }}, 1, {{ column_name }}) over (
             partition by {{ partition_by }} 
             order by {{ order_by }} asc
         )
--- models/marts/channels_multiple_trending_videos/sql/channels_trending_non_consecutive_days.sql
+++ models/marts/channels_multiple_trending_videos/sql/channels_trending_non_consecutive_days.sql
@@ -58,13 +58,26 @@ channels_multiple_trending_videos as (
 
 )
 
+, channels_videos_multiple_trending_gaps_dates as (
+
+    select
+
+        channel_id
+        , video_id
+        , trending_date
+        , lag(trending_date, 1, trending_date) over (partition by video_id order by trending_date) as previous_trending_date
+    
+    from channels_videos_multiple_trending_gaps
+
+)
+
 , final as (
 
     select
 
-        channels_videos_multiple_trending_gaps.channel_id
-        , channels_videos_multiple_trending_gaps.video_id
-        , channels_videos_multiple_trending_gaps.trending_date
+        channels_videos_multiple_trending_gaps_dates.channel_id
+        , channels_videos_multiple_trending_gaps_dates.video_id
+        , channels_videos_multiple_trending_gaps_dates.trending_date
 
         , array_size(tags) as tags_count
 
@@ -72,19 +85,24 @@ channels_multiple_trending_videos as (
         , comment_count
         , view_count
 
-        , {{ difference_from_previous('tags_count', 'channels_videos_multiple_trending_gaps.video_id', 'channels_videos_multiple_trending_gaps.trending_date')}} as diff_tags
+        , {{ difference_from_previous('tags_count', 'channels_videos_multiple_trending_gaps_dates.video_id', 'channels_videos_multiple_trending_gaps_dates.trending_date')}} as diff_tags
 
-        , {{ difference_from_previous('likes_count', 'channels_videos_multiple_trending_gaps.video_id', 'channels_videos_multiple_trending_gaps.trending_date')}} as diff_likes
+        , {{ difference_from_previous('likes_count', 'channels_videos_multiple_trending_gaps_dates.video_id', 'channels_videos_multiple_trending_gaps_dates.trending_date')}} as diff_likes
 
-        , {{ difference_from_previous('comment_count', 'channels_videos_multiple_trending_gaps.video_id', 'channels_videos_multiple_trending_gaps.trending_date')}} as diff_comments
+        , {{ difference_from_previous('comment_count', 'channels_videos_multiple_trending_gaps_dates.video_id', 'channels_videos_multiple_trending_gaps_dates.trending_date')}} as diff_comments
 
-        , {{ difference_from_previous('view_count', 'channels_videos_multiple_trending_gaps.video_id', 'channels_videos_multiple_trending_gaps.trending_date')}} as diff_views
+        , {{ difference_from_previous('view_count', 'channels_videos_multiple_trending_gaps_dates.video_id', 'channels_videos_multiple_trending_gaps_dates.trending_date')}} as diff_views
 
-    from channels_videos_multiple_trending_gaps
+        , datediff(day, previous_trending_date, channels_videos_multiple_trending_gaps_dates.trending_date) as days_of_gap_from_previous
+
+    from channels_videos_multiple_trending_gaps_dates
 
     left join int_channels_multiple_trending_videos
-        on channels_videos_multiple_trending_gaps.video_id = int_channels_multiple_trending_videos.video_id
-        and channels_videos_multiple_trending_gaps.trending_date = int_channels_multiple_trending_videos.trending_date
+        on channels_videos_multiple_trending_gaps_dates.video_id = int_channels_multiple_trending_videos.video_id
+        and channels_videos_multiple_trending_gaps_dates.trending_date = int_channels_multiple_trending_videos.trending_date
+    
+    where 1 = 1
+        and (days_of_gap_from_previous = 0 or days_of_gap_from_previous > 1)
 
 )
 
